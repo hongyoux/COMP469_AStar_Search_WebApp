047b6759c346b103a1deb6909b7e5fcdf26c4169
Hongyou Xiong <hongyoux@gmail.com>
2018-05-02 17:45:42 -0700

    Final version of the project

--- static/gridNode.js
+++ static/gridNode.js
@@ -4,13 +4,8 @@ class GridNode
 	{
 		this.x = x;
 		this.y = y;
-		this.cost = 1;
 		this.blocked = false;
 		this.visited = false;
-	}
-	SetCost(x)
-	{
-		this.cost = x;
 	}
 }
 
@@ -18,7 +13,7 @@ class Grid
 {
 	constructor(gridSize, canvas)
 	{
-
+		// Canvas Variables
 		this.canvas = canvas;
 		Grid.gridSize = gridSize;
 
@@ -30,11 +25,21 @@ class Grid
 			fromCenter: false,
 			cornerRadius: 1
 		};
-
 		this.nodes = [];
 		this.goal = null;
 		this.robot = null;
 		this.MakeNodes();
+
+		// A Star Variables
+		this.closedSet = new Set();
+		this.openSet = new Set();
+		this.cameFrom = new Map();
+		this.gScores = new Map();
+		this.fScores = new Map();
+		this.path = [];
+		this.drawnPath = [];
+		this.timeout = 50;
+		this.timeoutFunc = null;
 	}
 	MakeNodes()
 	{
@@ -57,6 +62,17 @@ class Grid
 			}
 		}
 	}
+	CreateErrorBox()
+	{
+		var html = '<div id="errorAlert" class="alert alert-danger alert-dismissible fade hide" role="alert"> \
+		<strong id="boldSummary"></strong> \
+		<span id="errorMsg"></span>\
+		<button type="button" class="close" data-dismiss="alert" aria-label="Close">\
+		<span aria-hidden="true">&times;</span>\
+		</button>\
+		</div>';
+		$("#errorLoc").append(html);
+	}
 	FromPixelToNode(x, y)
 	{
 		var xIndex = Math.floor(x / Grid.gridSize);
@@ -68,6 +84,11 @@ class Grid
 		var nodeIndex = y * (this.canvas.height() / Grid.gridSize) + x;
 		return this.nodes[nodeIndex];
 	}
+	GetNodeIndex(node)
+	{
+		return node.y * (this.canvas.height() / Grid.gridSize) + node.x
+	}
+
 	DrawNodes()
 	{
 		var self = this;
@@ -99,75 +120,12 @@ class Grid
 			this.DrawEmpty(nodeX, nodeY);
 		}
 	}
-	RespawnRandomTerrain()
-	{
-		var self = this;
-
-		var typedProb = parseFloat($("#spawnProb").val());
-
-		var prob = (isNaN(typedProb)) ? Grid.blockedProbability : typedProb; 
-
-		this.nodes.forEach(function(val, index)
-		{
-			if (val != self.robot || val != self.goal)
-			{
-				var isBlocked = Math.random() < prob;
-				val.blocked = isBlocked;
-			}
-		});
-		this.DrawNodes();
-	}
-	HighlightCurrentSquare(oldNode, newNode)
-	{
-		if (oldNode != null)
-		{
-			this.DrawNode(oldNode);
-		}
-
-		if (newNode.blocked || newNode == this.robot)
-		{
-			// Don't do anything if blocked or robot.
-			return;
-		}
-
-		var drawStyle = {
-			x: newNode.x * Grid.gridSize,
-			y: newNode.y * Grid.gridSize,
-			fillStyle: "#c8e6c9"
-		}
-		drawStyle = Object.assign(Grid.blockStyle, drawStyle);
-
-		this.canvas.drawRect(drawStyle);
-	}
-	SetGoal(node)
-	{
-		this.goal = node;
-		this.DrawNodes();
-	}
-	SetRandomRobot()
-	{
-		var self = this;
-		var x = Math.floor(Math.random() * (this.canvas.width() / Grid.gridSize));
-		var y = Math.floor(Math.random() * (this.canvas.height() / Grid.gridSize));
-
-		while (self.GetNode(x, y).blocked === true)
-		{
-			x = Math.floor(Math.random() * (self.canvas.width() / Grid.gridSize));
-			y = Math.floor(Math.random() * (self.canvas.height() / Grid.gridSize));
-		}
-
-		this.robot = this.GetNode(x, y);
-		this.DrawNodes();
-
-		var outStr = '[' + x + ',' + y + "]";
-		$("#currRobotText").text(outStr);
-	}
 	DrawRobot(nodeX, nodeY)
 	{
 		var drawStyle = {
 			x: nodeX,
 			y: nodeY,
-			fillStyle: "#d32f2f"
+			fillStyle: "#b71c1c"
 		}
 		drawStyle = Object.assign(Grid.blockStyle, drawStyle);
 
@@ -195,10 +153,6 @@ class Grid
 
 		this.canvas.drawRect(drawStyle);
 	}
-	DrawCost(nodeX, nodeY)
-	{
-
-	}
 	DrawEmpty(nodeX, nodeY)
 	{
 		var drawStyle = {
@@ -210,6 +164,368 @@ class Grid
 		drawStyle = Object.assign(Grid.blockStyle, drawStyle);
 
 		this.canvas.drawRect(drawStyle);
+	}
+	DrawPath()
+	{
+		var self = this;
+		this.drawnPath.forEach(function(val, index) {
+			var node = self.nodes[val];
+			var drawStyle = {
+				x: node.x * Grid.gridSize,
+				y: node.y * Grid.gridSize
+			}
+			switch(index)
+			{
+				default:
+				case 0:
+					drawStyle['fillStyle'] = "#c62828";
+					break;
+				case 1:
+					drawStyle['fillStyle'] = "#d32f2f";
+					break;
+				case 2:
+					drawStyle['fillStyle'] = "#e53935";
+					break;
+				case 3:
+					drawStyle['fillStyle'] = "#f44336";
+					break;
+			}
+			drawStyle = Object.assign(Grid.blockStyle, drawStyle);
+			self.canvas.drawRect(drawStyle);
+		});
+	}
+	DrawAStar()
+	{
+		if (this.goal == null)
+		{
+			this.CreateErrorBox();
+			$("#boldSummary").text("Missing Goal");
+			$("#errorMsg").text("Please click 'Assign Goal' and a square on the grid");
+			$("#errorAlert").toggleClass("hide show");
+			return;
+		}
+
+		this.ResetAStar();
+
+		this.path = this.GeneratePath();
+
+		if (this.path == null)
+		{
+			//Failed to find a path
+			this.CreateErrorBox();
+			$("#boldSummary").text("No Path");
+			$("#errorMsg").text("Could not Find a Path");
+			$("#errorAlert").toggleClass("hide show");
+			return;
+		}
+
+		var self = this;
+		setTimeout(function() {
+			self.DrawPathNode();
+		}, self.timeout);
+	}
+	DrawPathNode()
+	{
+		if (this.path.length > 0)
+		{
+			this.drawnPath.unshift(this.GetNodeIndex(this.robot));
+			this.robot = this.nodes[this.path.pop()];
+
+			if (this.drawnPath.length > 4)
+			{
+				this.drawnPath.pop();
+			}
+
+			this.DrawNodes();
+			this.DrawPath();
+
+			var self = this;
+			if (this.path.length > 0)
+			{
+				setTimeout(function() {
+					self.DrawPathNode();
+				}, self.timeout);
+			}
+			else
+			{
+				setTimeout(function() {
+					self.FinishTrail();
+				}, self.timeout);
+			}
+		}
+	}
+	FinishTrail()
+	{
+		this.drawnPath.pop();
+		this.DrawNodes();
+		this.DrawPath();
+
+		var self = this;
+		if (this.drawnPath.length > 0)
+		{
+			setTimeout(function() {
+				self.FinishTrail();
+			}, self.timeout);
+		}
+	}
+	ResetAStar()
+	{
+		this.closedSet.clear();
+		this.openSet.clear();
+		this.cameFrom.clear();
+		this.gScores.clear();
+		this.fScores.clear();
+		this.path = [];
+		this.drawnPath = [];
+	}
+	RespawnRandomTerrain()
+	{
+		var self = this;
+
+		var typedProb = parseFloat($("#spawnProb").val());
+
+		var prob = (isNaN(typedProb)) ? Grid.blockedProbability : typedProb; 
+
+		this.nodes.forEach(function(val, index)
+		{
+			if (val != self.robot || val != self.goal)
+			{
+				var isBlocked = Math.random() < prob;
+				val.blocked = isBlocked;
+			}
+		});
+		this.DrawNodes();
+	}
+	HighlightGoal(oldNode, newNode)
+	{
+		if (oldNode != null)
+		{
+			this.DrawNode(oldNode);
+		}
+
+		if (newNode.blocked || newNode == this.robot)
+		{
+			// Don't do anything if blocked or robot.
+			return;
+		}
+
+		var drawStyle = {
+			x: newNode.x * Grid.gridSize,
+			y: newNode.y * Grid.gridSize,
+			fillStyle: "#c8e6c9"
+		}
+		drawStyle = Object.assign(Grid.blockStyle, drawStyle);
+
+		this.canvas.drawRect(drawStyle);
+	}
+	HighlightBlock(oldNode, newNode)
+	{
+		if (oldNode != null)
+		{
+			this.DrawNode(oldNode);
+		}
+
+		if (newNode == this.goal || newNode == this.robot)
+		{
+			// Don't do anything if goal or robot.
+			return;
+		}
+
+		var drawStyle = {
+			x: newNode.x * Grid.gridSize,
+			y: newNode.y * Grid.gridSize,
+			fillStyle: "#6c757d"
+		}
+		drawStyle = Object.assign(Grid.blockStyle, drawStyle);
+
+		this.canvas.drawRect(drawStyle);
+	}
+	HighlightRobot(oldNode, newNode)
+	{
+		if (oldNode != null)
+		{
+			this.DrawNode(oldNode);
+		}
+
+		if (newNode == this.goal || newNode.blocked)
+		{
+			// Don't do anything if goal or robot.
+			return;
+		}
+
+		var drawStyle = {
+			x: newNode.x * Grid.gridSize,
+			y: newNode.y * Grid.gridSize,
+			fillStyle: "#e83e8c"
+		}
+		drawStyle = Object.assign(Grid.blockStyle, drawStyle);
+
+		this.canvas.drawRect(drawStyle);
+	}
+	SetGoal(node)
+	{
+		this.goal = node;
+		this.DrawNodes();
+	}
+	SetBlocked(node)
+	{
+		node.blocked = !node.blocked;
+		this.DrawNodes();
+	}
+	SetRobot(node)
+	{
+		this.robot = node;
+		this.DrawNodes();
+	}
+	SetRandomRobot()
+	{
+		var self = this;
+		var x = Math.floor(Math.random() * (this.canvas.width() / Grid.gridSize));
+		var y = Math.floor(Math.random() * (this.canvas.height() / Grid.gridSize));
+
+		while (self.GetNode(x, y).blocked === true)
+		{
+			x = Math.floor(Math.random() * (self.canvas.width() / Grid.gridSize));
+			y = Math.floor(Math.random() * (self.canvas.height() / Grid.gridSize));
+		}
+
+		this.robot = this.GetNode(x, y);
+		this.DrawNodes();
+
+		var outStr = '[' + x + ',' + y + "]";
+		$("#currRobotText").text(outStr);
+	}
+
+	GeneratePath()
+	{
+		var startIndex = this.GetNodeIndex(this.robot);
+		this.gScores.set(startIndex, 0);
+		this.fScores.set(startIndex, this.EstimateCost(this.robot, this.goal));
+		this.openSet.add(this.robot);
+
+		var self = this;
+
+		while (this.openSet.size > 0)
+		{
+			var current = self.FindNextNode(self.openSet);
+			var currentIndex = self.GetNodeIndex(current);
+
+			if (current == self.goal)
+			{
+				return self.PathToGoal();
+			}
+
+			self.openSet.delete(current);
+			self.closedSet.add(current);
+
+			var neighbors = self.GetNeighbors(current);
+			neighbors.forEach(function(neighbor, index)
+			{
+				if (neighbor.blocked == true)
+				{
+					// Don't evaluate bad nodes
+					return;
+				}
+				if (self.closedSet.has(neighbor))
+				{
+					return;
+				}
+				if (!self.openSet.has(neighbor))
+				{
+					self.openSet.add(neighbor);
+				}
+
+				var neighborIndex = self.GetNodeIndex(neighbor);
+
+				var currGScore = (self.gScores.has(neighborIndex)) ? self.gScores.get(neighborIndex) : Infinity;
+				var newGScore = self.gScores.get(currentIndex) + self.EstimateCost(current, neighbor);
+				if (newGScore >= currGScore)
+				{
+					return;
+				}
+
+				self.cameFrom.set(neighborIndex, currentIndex);
+				self.gScores.set(neighborIndex, newGScore);
+				self.fScores.set(neighborIndex, self.gScores.get(neighborIndex) + self.EstimateCost(neighbor, self.goal));
+			});
+		}
+
+		return null;
+	}
+	PathToGoal()
+	{
+		var self = this;
+		var path = [];
+		var currNodeIndex = this.GetNodeIndex(this.goal);
+		var robotIndex = this.GetNodeIndex(this.robot);
+
+		while (currNodeIndex != robotIndex)
+		{
+			path.push(currNodeIndex);
+			currNodeIndex = self.cameFrom.get(currNodeIndex);
+		}
+		console.log("DONE");
+		return path;
+	}
+	EstimateCost(start, end)
+	{
+		//Octile distance
+
+		//Moving Up Down Left Right is 1 cost
+		//Moving Diagonally is sqrt(2) cost
+
+		//Moving diagonally means removing two straight costs
+		//You move diagonally dx or dy times, whichever is smaller.
+		var dx = Math.abs(start.x - end.x);
+		var dy = Math.abs(start.y - end.y);
+		var strCost = 1;
+		var diagCost = Math.sqrt(2);
+		return strCost * (dx + dy) + (diagCost - 2 * strCost) * Math.min(dx, dy);
+	}
+	FindNextNode(openSet)
+	{
+		var self = this;
+		var nextNode = null;
+		var nextNodeFValue = Infinity;
+		openSet.forEach(function(val, sameVal, openSet)
+		{
+			var valIndex = self.GetNodeIndex(val);
+			var valFScore = self.fScores.get(valIndex);
+			if (valFScore < nextNodeFValue)
+			{
+				nextNode = val;
+				nextNodeFValue = valFScore;
+			}
+		});
+		return nextNode;
+	}
+	GetNeighbors(node)
+	{
+		var list = [];
+		for (var i = -1; i < 2; i++)
+		{
+			for (var j = -1; j < 2; j++)
+			{
+				if (i == 0 && j == 0)
+				{
+					//Ignore self.
+					continue;
+				}
+
+				var newX = node.x + i;
+				var newY = node.y + j;
+
+				if (newX < 0 || newX >= this.canvas.width() / Grid.gridSize ||
+					newY < 0 || newY >= this.canvas.height() / Grid.gridSize)
+				{
+					//Invalid index
+					continue;
+				}
+
+				list.push(this.GetNode(newX, newY));
+			}
+		}
+		return list;
 	}
 }
 
--- static/main.js
+++ static/main.js
@@ -1,5 +1,5 @@
 $( document ).ready(function() {
-	InitBootstrapPlugins();
+	InitBootstrapFunctions();
 
     var gridSize = 20; // 16 square with 4 pixel border
 
@@ -10,6 +10,42 @@ $( document ).ready(function() {
 
     currentNode = null;
 
+    InitCanvasFunctions(grid);
+    InitMenuFunctions(grid);
+});
+
+function InitBootstrapFunctions()
+{
+	$('[data-toggle="tooltip"]').tooltip();
+
+    $('#spawnObjs .btn').on('click', function () {
+	    // toggle style
+	    $(this).siblings().removeClass("btn-secondary active").addClass("btn-outline-secondary");
+	    $(this).toggleClass('btn-secondary btn-outline-secondary active');
+	    
+	    // toggle checkbox
+	    var $chk = $(this).siblings().find('[type=checkbox]');
+	    $chk.prop('checked', false);
+
+	    var $thisCheckbox = $(this).find('[type=checkbox]')
+	    $thisCheckbox.prop('checked', !$thisCheckbox.prop('checked'));
+	    
+	    return false;
+	});
+
+	$('#hoverModeBtn').on('click', function () {
+		// toggle style
+	    $(this).toggleClass('btn-primary btn-outline-primary active');
+		
+		var $thisCheckbox = $(this).find('[type=checkbox]')
+	    $thisCheckbox.prop('checked', !$thisCheckbox.prop('checked'));
+	    
+	    return false;
+	});
+};
+
+function InitCanvasFunctions(grid)
+{
     $("#mainCanvas").mousemove(function(event) {
     	var xLoc = event.pageX - $(this).offset().left;
     	var yLoc = event.pageY - $(this).offset().top;
@@ -20,12 +56,24 @@ $( document ).ready(function() {
 	    	if ($("#pickGoal").is(':checked'))
 	    	{
 	    		//Highlight light-green
-	    		grid.HighlightCurrentSquare(currentNode, newNode);
+	    		grid.HighlightGoal(currentNode, newNode);
 	    	}
+	    	else if ($("#pickBlock").is(':checked'))
+	    	{
+	    		grid.HighlightBlock(currentNode, newNode);
+	    	}
+	    	else if ($("#pickRobot").is(':checked'))
+	    	{
+	    		grid.HighlightRobot(currentNode, newNode);
+	    	}
+    		
     		currentNode = newNode;
 
-	    	//Debug For Mouse Over
-	        console.log("mouse over: " + currentNode.x + ", " + currentNode.y);
+    		if ($('#hoverMode').is(':checked') && !currentNode.blocked && !(currentNode == grid.robot))
+    		{
+    			grid.SetGoal(currentNode);
+    			grid.DrawAStar();
+    		}
     	}
     });
 
@@ -41,26 +89,46 @@ $( document ).ready(function() {
 	    		grid.SetGoal(hoveredNode);
 	    	}
 	    	$("#pickGoalBtn").click();
+		}
+    	if ($("#pickBlock").is(':checked'))
+    	{
+	    	var xLoc = event.pageX - $(this).offset().left;
+	    	var yLoc = event.pageY - $(this).offset().top;
+
+	    	var hoveredNode = grid.FromPixelToNode(xLoc, yLoc);
+	    	if (hoveredNode != grid.robot && hoveredNode != grid.goal)
+	    	{
+	    		grid.SetBlocked(hoveredNode);
+	    	}
+    	}
+    	if ($("#pickRobot").is(':checked'))
+    	{
+	    	var xLoc = event.pageX - $(this).offset().left;
+	    	var yLoc = event.pageY - $(this).offset().top;
+
+	    	var hoveredNode = grid.FromPixelToNode(xLoc, yLoc);
+	    	if (hoveredNode != grid.goal && hoveredNode.blocked == false)
+	    	{
+	    		grid.SetRobot(hoveredNode);
+	    	}
+	    	$("#pickRobotBtn").click();
     	}
     });
+};
 
+function InitMenuFunctions(grid)
+{
     $("#randomBlockers").click(function(event) {
     	grid.RespawnRandomTerrain();
     });
-});
+    $("#runAlgo").click(function(event) {
+    	//Debug
+    	//console.log("Single step clicked!");
+    	if (grid.robot == grid.goal)
+    	{
+    		grid.goal = null;
+    	}
+    	grid.DrawAStar();
+    });
 
-function InitBootstrapPlugins()
-{
-	$('[data-toggle="tooltip"]').tooltip();
-
-    $('[data-toggle="buttons"] .btn').on('click', function () {
-	    // toggle style
-	    $(this).toggleClass('btn-secondary btn-primary active');
-	    
-	    // toggle checkbox
-	    var $chk = $(this).find('[type=checkbox]');
-	    $chk.prop('checked',!$chk.prop('checked'));
-	    
-	    return false;
-	});
 }
